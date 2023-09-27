64ee4c67eff726e192b53ddcd7106225072f47e3
Kjoonyoung <jy981110@gmail.com>
2023-09-27 16:00:19 +0900

    sql. 서비스 커스텀 추가

---
+++ backend/src/main/resources/docs/EMP/EMP.sql
@@ -0,0 +1,27 @@
+DROP table IF EXISTS `final`.EMP;
+
+create table `final`.EMP (
+	`EMP_ID`            BIGINT       NOT NULL AUTO_INCREMENT COMMENT '사원코드', -- 사원코드
+	`EMP_DEPT_ID`       BIGINT       NOT NULL COMMENT '부서코드', -- 부서코드
+	`EMP_NAME`          VARCHAR(255) NOT NULL COMMENT '이름', -- 이름
+	`EMP_EMAIL`         VARCHAR(255) NOT NULL COMMENT '이메일', -- 이메일
+	`EMP_PASSWORD`      VARCHAR(255) NOT NULL COMMENT '비밀번호', -- 비밀번호
+	`EMP_PHONENUMBER`   VARCHAR(255) NOT NULL COMMENT '휴대폰번호', -- 휴대폰번호
+	`EMP_POSITION`      VARCHAR(255) NOT NULL COMMENT '직급', -- 직급
+	`EMP_ROLES`         VARCHAR(255) NOT NULL COMMENT '권한', -- 권한
+	`EMP_BIRTHDAY`      DATE         NOT NULL COMMENT '생년월일', -- 생년월일
+	`EMP_STARTDATE`     DATE         NOT NULL COMMENT '입사일', -- 입사일
+	`EMP_ENDDATE`       DATE         NULL     COMMENT '퇴사일', -- 퇴사일
+	`EMP_STATUS`        VARCHAR(255) NOT NULL COMMENT '재직상태', -- 재직상태
+	`EMP_GENDER`        VARCHAR(255) NOT NULL COMMENT '성별', -- 성별
+	`EMP_ADDRESS`       VARCHAR(255) NOT NULL COMMENT '주소', -- 주소
+	`EMP_DETAILADDRESS` VARCHAR(255) NULL     COMMENT '상세주소', -- 상세주소
+	PRIMARY KEY (`EMP_ID`)
+) ENGINE = InnoDB COMMENT '사원' AUTO_INCREMENT = 101;
+
+-- EMP_EMAIL 유니크 키 추가
+ALTER TABLE `final`.EMP
+ADD CONSTRAINT `UK_EMP_EMAIL` UNIQUE (`EMP_EMAIL`);
+
+ALTER TABLE `final`.EMP
+ADD CONSTRAINT `FK_EMP_DEPT` FOREIGN KEY (`EMP_DEPT_ID`) REFERENCES `final`.DEPT (`DEPT_ID`);
---
+++ backend/src/main/resources/docs/EMP/LOG.sql
@@ -0,0 +1,21 @@
+-- 사원근태
+DROP TABLE IF EXISTS `final`.LOG;
+CREATE TABLE `final`.LOG
+(
+    `LOG_ID`            BIGINT       NOT NULL AUTO_INCREMENT COMMENT '근태코드',
+    `LOG_EMP_ID`        BIGINT       NOT NULL COMMENT '사원코드',
+    `LOG_DATE`          DATE         NOT NULL COMMENT '날짜',
+    `LOG_CHECKIN`       DATETIME     NULL COMMENT '출근시간',
+    `LOG_CHECKOUT`      DATETIME     NULL COMMENT '퇴근시간',
+    `LOG_STATUS`        VARCHAR(255) NOT NULL COMMENT '근태상태',
+    `LOG_TOTALVACATION` INT          NOT NULL COMMENT '총 휴가',
+    `LOG_USEDVACATION`  INT          NOT NULL COMMENT '사용 휴가',
+    `LOG_TOTALDAYOFF`   INT          NOT NULL COMMENT '총 연차',
+    `LOG_USEDDAYOFF`    INT          NOT NULL COMMENT '사용 연차',
+    `LOG_DATEDAYOFF`    DATE         NULL COMMENT '연차사용일',
+    PRIMARY KEY (`LOG_ID`)
+) ENGINE = InnoDB COMMENT '사원근태'
+  AUTO_INCREMENT = 1;
+
+ALTER TABLE `final`.LOG
+    ADD CONSTRAINT `FK_LOG_EMP` FOREIGN KEY (`LOG_EMP_ID`) REFERENCES `final`.EMP (`EMP_ID`);
