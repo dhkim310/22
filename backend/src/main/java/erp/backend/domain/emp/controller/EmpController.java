ea006b4647ac0b5c2a8cebc54005d6331b39572e
김지원 <power852456@naver.com>
2023-10-04 10:53:29 +0900

    domain entity!

--- backend/src/main/java/erp/backend/domain/emp/controller/EmpController.java
+++ backend/src/main/java/erp/backend/domain/emp/controller/EmpController.java
@@ -2,7 +2,8 @@ package erp.backend.domain.emp.controller;
 
 import erp.backend.domain.emp.dto.SignInRequest;
 import erp.backend.domain.emp.dto.SignInResponse;
-import erp.backend.domain.emp.service.UserService;
+import erp.backend.domain.emp.dto.SignUpRequest;
+import erp.backend.domain.emp.service.EmpService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/emp")
 public class EmpController {
-    private final UserService userService;
     private final EmpService empService;
 
     @PostMapping("/sign-in")
     public ResponseEntity<SignInResponse> signIn(@RequestBody @Valid SignInRequest request) {
         System.out.println(request.toString());
-        return ResponseEntity.ok(userService.signIn(request));
+        return ResponseEntity.ok(empService.signIn(request));
+    }
 
     @PostMapping("/sign-up")
     public ResponseEntity<Boolean> signUp(@RequestBody @Valid SignUpRequest request) {
