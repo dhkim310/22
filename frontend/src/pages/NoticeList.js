f44a92aa1a5716d357e3e1026013a1bd5c74ea2d
jaehyeong <qwer233104@icloud.com>
2023-10-12 03:18:54 +0900

    log 로직 수정

--- frontend/src/pages/NoticeList.js
+++ frontend/src/pages/NoticeList.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import '../assets/bootstrap/css/bootstrap.min.css';
 import '../assets/css/animate.min.css'
+import { useNavigate } from 'react-router-dom';
 
 function NoticeList() {
     const [isMobile, setIsMobile] = useState(false);
     const [hoverAnimationList, setHoverAnimationList] = useState([]);
+
+    const navigate = useNavigate();
+        const navigateToWrite = () => {
+            navigate("/write");
+        };
 
     useEffect(() => {
         const getWidth = () => {
@@ -29,7 +35,7 @@ function NoticeList() {
     return (
     <div>
             <div className="d-xxl-flex justify-content-xxl-center" style={{width: '100%', height: '1800px', background: 'transparent'}}>
-              <div className="d-xxl-flex justify-content-xxl-center" style={{width: '10%', height: '100%', background: 'white'}}><button className="btn btn-primary d-xxl-flex justify-content-xxl-center align-items-xxl-center" data-bss-hover-animate="pulse" type="button" style={{width: '150px', height: '50px', color: 'black', background: 'rgba(13,110,253,0)', borderRadius: '6px', borderColor: 'black', marginTop: '24px'}}>글쓰기</button></div>
+              <div className="d-xxl-flex justify-content-xxl-center" style={{width: '10%', height: '100%', background: 'white'}}><button className="btn btn-primary d-xxl-flex justify-content-xxl-center align-items-xxl-center" data-bss-hover-animate="pulse" type="button" onClick={ navigateToWrite } style={{width: '150px', height: '50px', color: 'black', background: 'rgba(13,110,253,0)', borderRadius: '6px', borderColor: 'black', marginTop: '24px'}}>글쓰기</button></div>
               <div style={{background: 'white', width: '90%', height: '100%'}}>
                 <div style={{width: '100%', height: '7%', borderBottom: '2px ridge rgba(128,128,128,0.26)'}}>
                   <div className="d-xxl-flex align-items-xxl-end" style={{width: '100%', height: '50%'}}><span style={{fontSize: '30px', fontWeight: 'bold', paddingTop: '0px', marginLeft: '41px'}}>게시판 홈</span></div>
