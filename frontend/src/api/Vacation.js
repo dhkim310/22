f7e5a595ba238e6bc059abd32b60e28f2bf45d2d
yongmin <lol3347@naver.com>
2023-11-03 11:44:53 +0900

    휴가 업데이트?

--- frontend/src/api/Vacation.js
+++ frontend/src/api/Vacation.js
@@ -8,14 +8,6 @@ export const vacationDetail = async (id) => {
         throw error;
     }
 }
-// export const vacationUpdate = async (params) => {
-//     try{
-//         const response = await axios.put('/api/vacation', params);
-//         return response;
-//     }catch (error){
-//         throw error;
-//     }
-// }
 
 export const vacationUpdate = async (id, {updateData}) => {
     return new Promise((resolve, reject) => {
