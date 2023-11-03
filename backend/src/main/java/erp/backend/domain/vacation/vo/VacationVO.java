e30fa21afcf2a619a2b5578734b60c173558a7f6
yongmin <lol3347@naver.com>
2023-11-03 12:30:57 +0900

    휴가 업데이트?

--- backend/src/main/java/erp/backend/domain/vacation/vo/VacationVO.java
+++ backend/src/main/java/erp/backend/domain/vacation/vo/VacationVO.java
@@ -8,5 +8,9 @@ public class VacationVO {
     public int differenceInDays(int endDate, int startDate){
         return Math.round((endDate - startDate) / DAY);
     }
-
+    // 휴가 더 많이 들어갈 때
+    // 둘 중에 하나 ( 연차 or 휴가 )
+    // 컴포넌트 창에 사용일로 바꾸기
+    // 시작일이 더 앞에 있으면 안되고
+    // 헤더바꾸기
 }
