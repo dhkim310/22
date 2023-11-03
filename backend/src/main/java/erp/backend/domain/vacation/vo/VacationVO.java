e482767180c52d14dcbd9cd207d92802cfa2304c
jaehyeong <qwer233104@icloud.com>
2023-11-03 12:38:45 +0900

    autoFocus

--- backend/src/main/java/erp/backend/domain/vacation/vo/VacationVO.java
+++ backend/src/main/java/erp/backend/domain/vacation/vo/VacationVO.java
@@ -4,5 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class VacationVO {
-
+    private final int DAY =  (24 * 60 * 60 * 1000);
+    public int differenceInDays(int endDate, int startDate) {
+        return Math.round((endDate -  startDate) / DAY);
+    }
 }
