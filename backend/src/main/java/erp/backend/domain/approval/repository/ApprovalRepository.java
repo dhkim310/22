be41bc94dad106a205acfba9a2d9acd6ad15a10d
YuJaeHyeong <qwer233104@icloud.com>
2023-11-06 05:01:43 +0900

    tets

--- backend/src/main/java/erp/backend/domain/approval/repository/ApprovalRepository.java
+++ backend/src/main/java/erp/backend/domain/approval/repository/ApprovalRepository.java
@@ -1,7 +1,6 @@
 package erp.backend.domain.approval.repository;
 
 import erp.backend.domain.approval.entity.Approval;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
